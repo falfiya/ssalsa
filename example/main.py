970ef9adf6c9f8605256f1fe54105b426e0e9d53
falfia <22552895+falfiya@users.noreply.github.com>
2026-08-16 16:56:37 -0700

    Make example harder

--- example/main.py
+++ example/main.py
@@ -3,25 +3,41 @@ import random
 
 s = ssalsa.Runtime[None]()
 
-
-@s.input
-def a():
-   return random.randint(1, 9), None
-
-
-@s.input
-def b():
-   return random.randint(1, 9), None
-
+a = s.create_input()
+b = s.create_input()
 
 @s.tracked
 def c():
+   print(f"c() = {a()} + {b()} = {a() + b()}")
    return a() + b()
 
 
 @s.tracked
-def times_two():
-   return c() * 2
+def d(x):
+   print(f"d({x}) = {x} // 2 = {x // 2}")
+   return x // 2
 
+@s.tracked
+def e():
+   print(f"e() = d(c()) = d({c()}) = {d(c())}")
+   return d(c())
 
-print(times_two.get_memo())
+@s.tracked
+def print_it():
+   print(f"The result was {e()}")
+
+a.set(1, None)
+b.set(2, None)
+print_it()
+# Full Compute
+#     c() = 1 + 2 = 3
+#     d(3) = 3 // 2 = 1
+#     e() = d(c()) = d(3) = 1
+#     The result was 1
+b.set(1, None)
+# Partial compute
+#     c() = 1 + 1 = 2
+#     d(2) = 2 // 2 = 1
+#     e() = d(c()) = d(2) = 1
+# Does not print_it!
+print_it()
